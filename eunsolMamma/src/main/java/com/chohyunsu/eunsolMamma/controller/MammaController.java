b5148b78c9e3058f4472ddc7d628fd1460bf4502
hyunsu <fernweh6990@gmail.com>
2018-01-29 21:19:02 +0900

    1차 수정

--- eunsolMamma/src/main/java/com/chohyunsu/eunsolMamma/controller/MammaController.java
+++ eunsolMamma/src/main/java/com/chohyunsu/eunsolMamma/controller/MammaController.java
@@ -10,13 +10,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.chohyunsu.eunsolMamma.model.MammaDetail;
 import com.chohyunsu.eunsolMamma.service.*;
 
+
 @Controller
 public class MammaController {
 
+
+	private static Logger logger = LoggerFactory.getLogger(MammaController.class);
 	@Autowired
 	private MammaService mammaService;
 
